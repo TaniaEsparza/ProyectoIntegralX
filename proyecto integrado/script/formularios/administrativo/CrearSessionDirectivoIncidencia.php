0f8ed4fbc5b404fdf792d2b924fbe234cba611a5
TaniaEsparza <31520735+TaniaEsparza@users.noreply.github.com>
2019-12-04 11:30:20 -0600

    modificacion
    Elimine echo del id en clase crearsessiondirectivoincidencia y crearsessiondirectivoinvetario

--- proyecto integrado/script/formularios/administrativo/CrearSessionDirectivoIncidencia.php
+++ proyecto integrado/script/formularios/administrativo/CrearSessionDirectivoIncidencia.php
@@ -2,7 +2,7 @@
 if (!isset($_SESSION)) { session_start(); }
 
 $_SESSION['idIncidencia'] = $_GET['idIncidencia'];
-echo "La session tiene el numero".$_SESSION['idIncidencia'];
+//echo "La session tiene el numero".$_SESSION['idIncidencia'];
 echo "<SCRIPT>window.location='ModificacionIncidencia.php';</SCRIPT>";
 
 ?>
--- proyecto integrado/script/formularios/administrativo/CrearSessionDirectivoInventario.php
+++ proyecto integrado/script/formularios/administrativo/CrearSessionDirectivoInventario.php
@@ -2,7 +2,7 @@
 if (!isset($_SESSION)) { session_start(); }
 
 $_SESSION['idInventario'] = $_GET['idInventario'];
-echo "La session tiene el numero".$_SESSION['idInventario'];
+//echo "La session tiene el numero".$_SESSION['idInventario'];
 echo "<SCRIPT>window.location='ModificacionInventario.php';</SCRIPT>";
 
 ?>
